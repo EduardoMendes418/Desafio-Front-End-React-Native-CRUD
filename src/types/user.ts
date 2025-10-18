c77ad8765e60451ad6777a85615726cc5d61819f
eduardo <eduardomendes418@gmail.com>
2025-10-18 16:32:03 -0300

    [RNC-13] feat - create types

---
+++ src/types/user.ts
@@ -0,0 +1,15 @@
+// types/user.ts
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  website: string;
+}
+
+export interface UserFormData {
+  name: string;
+  email: string;
+  phone: string;
+  website: string;
+}
