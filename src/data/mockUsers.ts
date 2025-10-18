0b5cddbb9fc6e698e507f168118d31ab4c219657
eduardo <eduardomendes418@gmail.com>
2025-10-18 16:32:20 -0300

    [RNC-14] feat - create mocks

---
+++ src/data/mockUsers.ts
@@ -0,0 +1,74 @@
+import { User } from '../types/user';
+
+export const mockUsers: User[] = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    email: "leanne@april.biz",
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org",
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    email: "ervin@melissa.tv",
+    phone: "010-692-6593 x09125",
+    website: "anastasia.net",
+  },
+  {
+    id: 3,
+    name: "Clementine Bauch",
+    email: "clementine@yesenia.net",
+    phone: "1-463-123-4447",
+    website: "ramiro.info",
+  },
+  {
+    id: 4,
+    name: "Patricia Lebsack",
+    email: "patricia@kory.org",
+    phone: "493-170-9623 x156",
+    website: "kale.biz",
+  },
+  {
+    id: 5,
+    name: "Chelsey Dietrich",
+    email: "chelsey@annie.ca",
+    phone: "(254)954-1289",
+    website: "demarco.info",
+  },
+  {
+    id: 6,
+    name: "Dennis Schulist",
+    email: "dennis@jasper.info",
+    phone: "1-477-935-8478 x6430",
+    website: "ola.org",
+  },
+  {
+    id: 7,
+    name: "Kurtis Weissnat",
+    email: "kurtis@annie.ca",
+    phone: "210-067-6132",
+    website: "elvis.io",
+  },
+  {
+    id: 8,
+    name: "Nicholas Runolfsdottir",
+    email: "nicholas@runolfsdottir.com",
+    phone: "586-493-6943",
+    website: "jacynthe.com",
+  },
+  {
+    id: 9,
+    name: "Glenna Reichert",
+    email: "glenna@reichert.org",
+    phone: "775-976-6794",
+    website: "conrad.com",
+  },
+  {
+    id: 10,
+    name: "Clementina DuBuque",
+    email: "clementina@dubuque.net",
+    phone: "024-648-3804",
+    website: "ambrose.net",
+  }
+];
